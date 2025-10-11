8069253a72bec022c7da734ad35038b627f41ad7
Tim Monko <timmonko@gmail.com>
2025-10-11 09:38:41 -0500

    remove template widget

--- src/ndevio/_widget.py
+++
@@ -1,8 +0,0 @@
-"""
-Widgets for ndevio.
-
-TODO: Implement actual ndevio widgets for:
-- Reader settings and configuration UI
-- Plugin manager integration (install bioio readers)
-- Export/screenshot utilities
-"""
